03afb9409044ffecda2bb81b018706ac0fb274a5
Richard Collier <ergosoftuk@gmail.com>
2017-09-05 11:34:39 +0100

    Setup end-to-end testing

--- FrontEnd/e2e/app.e2e-spec.ts
+++ FrontEnd/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AngularPage } from './app.po';
+import {$, browser, by, element} from "protractor";
 
 describe('angular App', () => {
   let page: AngularPage;
@@ -7,9 +8,24 @@ describe('angular App', () => {
     page = new AngularPage();
   });
 
-  it('should display welcome message', done => {
+  it ('Initialize', () => {
     page.navigateTo();
-    page.getParagraphText()
+
+    // Wait for login.
+    browser.wait(browser.ExpectedConditions.urlContains('/auth/realms/endeavour/protocol/openid-connect'));
+  });
+
+  it ('Perform login', () => {
+    element(by.name('username')).sendKeys('e2etest');
+    element(by.name('password')).sendKeys('e2eTestPass');
+    element(by.name('login')).click();
+
+    // Wait for main app page
+    browser.wait(() => $('#content').isPresent());
+  });
+
+  it ('Check page loaded', done => {
+    element(by.className('title-text')).getText()
       .then(msg => expect(msg).toEqual('Information Modeller'))
       .then(done, done.fail);
   });
