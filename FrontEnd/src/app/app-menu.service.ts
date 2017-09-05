c1232c784cc7e0ac8b1868d01e689ae284bbf45b
Richard Collier <ergosoftuk@gmail.com>
2017-09-05 13:41:46 +0100

    Fix client-id and roles

--- FrontEnd/src/app/app-menu.service.ts
+++ FrontEnd/src/app/app-menu.service.ts
@@ -5,15 +5,15 @@ import {MenuOption} from "eds-angular4/dist/layout/models/MenuOption";
 @Injectable()
 export class AppMenuService implements  AbstractMenuProvider {
   getClientId(): string {
-    return 'eds-information-modeller';
+    return 'eds-info-manager';
   }
   getApplicationTitle(): string {
     return 'Information Modeller';
   }
   getMenuOptions():MenuOption[] {
     return [
-      {caption: 'Concept Modeller', state: 'conceptModeller', icon: 'fa fa-sitemap'},
-      {caption: 'IM Settings', state: 'settings', icon: 'fa fa-cogs' }
+      {caption: 'Concept Modeller', state: 'conceptModeller', icon: 'fa fa-sitemap', role: 'eds-info-manager:modeller'},
+      {caption: 'IM Settings', state: 'settings', icon: 'fa fa-cogs', role: 'eds-info-manager:settings' }
     ];
   }
 }
