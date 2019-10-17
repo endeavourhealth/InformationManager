b262d9c8bbb5049df1eaebaf81dc9e875cea8737
JamesBiswellEnd <james.biswell@endeavourhealth.org>
2019-10-17 16:47:27 +0100

    Adding to data model classes of query module.

--- FrontEndv8/src/app/query/models/Parameter.ts
+++ FrontEndv8/src/app/query/models/Parameter.ts
@@ -1,3 +1,14 @@
+import {Function} from "./Function";
+
 export class Parameter {
+
+  property: string;
+  definedProperty: string;
+  parameterToken: string;
+  variable: string;
+  valueDateTime: any;
+  valueNumeric: number;
+  function: Function;
+  parameterName: string;
 
 }
--- common/src/main/java/org/endeavourhealth/informationmanager/common/querymodels/Parameter.java
+++ common/src/main/java/org/endeavourhealth/informationmanager/common/querymodels/Parameter.java
@@ -1,4 +1,82 @@
 package org.endeavourhealth.informationmanager.common.querymodels;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+// (not needed) import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Parameter {
+
+    private String property;
+    private String definedProperty;
+    private String parameterToken;
+    private String variable;
+    private Object valueDateTime;
+    private Float valueNumeric;
+    private Function function;
+    private String parameterName;
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public String getDefinedProperty() {
+        return definedProperty;
+    }
+
+    public void setDefinedProperty(String definedProperty) {
+        this.definedProperty = definedProperty;
+    }
+
+    public String getParameterToken() {
+        return parameterToken;
+    }
+
+    public void setParameterToken(String parameterToken) {
+        this.parameterToken = parameterToken;
+    }
+
+    public String getVariable() {
+        return variable;
+    }
+
+    public void setVariable(String variable) {
+        this.variable = variable;
+    }
+
+    public Object getValueDateTime() {
+        return valueDateTime;
+    }
+
+    public void setValueDateTime(Object valueDateTime) {
+        this.valueDateTime = valueDateTime;
+    }
+
+    public Float getValueNumeric() {
+        return valueNumeric;
+    }
+
+    public void setValueNumeric(Float valueNumeric) {
+        this.valueNumeric = valueNumeric;
+    }
+
+    public Function getFunction() {
+        return function;
+    }
+
+    public void setFunction(Function function) {
+        this.function = function;
+    }
+
+    public String getParameterName() {
+        return parameterName;
+    }
+
+    public void setParameterName(String parameterName) {
+        this.parameterName = parameterName;
+    }
+
 }
