b77b4806650cd221615adcba70bdbcddf8285d75
Richard Collier <ergosoftuk@gmail.com>
2019-10-29 14:11:03 +0000

    Path fixes for sub path deployments

--- FrontEndv8/src/app/app.module.ts
+++ FrontEndv8/src/app/app.module.ts
@@ -30,7 +30,7 @@ const keycloakService = new KeycloakService();
 export class AppModule implements DoBootstrap {
   ngDoBootstrap(appRef: ApplicationRef) {
     keycloakService
-      .init({config: '/public/wellknown/authconfigraw', initOptions: {onLoad: 'login-required'}})
+      .init({config: 'public/wellknown/authconfigraw', initOptions: {onLoad: 'login-required'}})
       .then((authenticated) => {
         if (authenticated)
           appRef.bootstrap(LayoutComponent);
