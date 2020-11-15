134154d71425c62b67a401da229a4aaf3ed4ffb5
David Stables <DavidStables@users.noreply.github.com>
2020-11-15 13:34:13 +0000

    Run time importer deprecated

--- RuntimeImporter/src/main/java/org/endeavourhealth/informationManager/Main.java
+++
@@ -1,8 +0,0 @@
-package org.endeavourhealth.informationManager;
-
-public class Main {
-
-    public static void main(String[] argv) throws Exception {
-        new RuntimeImporter().execute();
-    }
-}
