684cdb11ad6b6df91c3d1494e74afa2c62c3a3de
Richard Collier <gitkraken@tirinoarim.co.uk>
2021-01-25 13:47:41 +0000

    v1-v2 migration work

---
+++ Database/5.SupplierMaps/2.tpp-maps-import.sql
@@ -0,0 +1,17 @@
+USE supplier_maps;
+
+-- ********************* CONCEPT *********************
+
+DROP TABLE IF EXISTS tpp_maps;
+CREATE TABLE tpp_maps (
+    ctv3_code VARCHAR(20) COLLATE utf8_bin NOT NULL,
+    ctv3_term VARCHAR(400),
+    snomed_concept_id BIGINT,
+    is_tpp_code BOOLEAN
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Uploads\\SupplierMaps\\tpp_maps.csv'
+    INTO TABLE tpp_maps
+    FIELDS TERMINATED BY '\t'
+    LINES TERMINATED BY '\r\n'
+    IGNORE 1 LINES;
