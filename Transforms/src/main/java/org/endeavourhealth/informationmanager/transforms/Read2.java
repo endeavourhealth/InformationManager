f20f6deba846e7b442352ffeb0fd6442ab979c85
Richard Collier <gitkraken@tirinoarim.co.uk>
2021-03-18 13:02:17 +0000

    Prep work

--- Transforms/src/main/java/org/endeavourhealth/informationmanager/transforms/Read2.java
+++ Transforms/src/main/java/org/endeavourhealth/informationmanager/transforms/Read2.java
@@ -11,12 +11,12 @@ import java.util.stream.Stream;
 public class Read2 {
     private static final String concepts = ".*\\\\READ\\\\DESC\\.csv";
     private static final String synonyms = ".*\\\\READ\\\\Term\\.csv";
-    private static final String maps = ".*\\\\SNOMED\\\\Mapping Tables\\\\Updated\\\\Clinically Assured\\\\rcsctmap2_uk_.*\\.txt";
-    private static final String altmaps = ".*\\\\SNOMED\\\\Mapping Tables\\\\Updated\\\\Clinically Assured\\\\codesWithValues_AlternateMaps_READ2_.*\\.txt";
+    // private static final String maps = ".*\\\\SNOMED\\\\Mapping Tables\\\\Updated\\\\Clinically Assured\\\\rcsctmap2_uk_.*\\.txt";
+    // private static final String altmaps = ".*\\\\SNOMED\\\\Mapping Tables\\\\Updated\\\\Clinically Assured\\\\codesWithValues_AlternateMaps_READ2_.*\\.txt";
 
     public static void main(String[] argv) throws IOException {
         if (argv.length != 1) {
-            System.err.println("You need to provide the path to the READ2 data files!");
+            System.err.println("You need to provide a root path containing READ2 data and SNOMED map files!");
             System.exit(-1);
         }
 
@@ -24,19 +24,20 @@ public class Read2 {
     }
 
     private static void validateFiles(String path) throws IOException {
-        String[] files =  Stream.of(concepts, synonyms, maps, altmaps)
+        String[] files =  Stream.of(concepts, synonyms) // , maps, altmaps)
             .toArray(String[]::new);
 
         for(String file: files) {
             try {
-                findFilesForId(path, file);
+                Path p = findFileForId(path, file);
+                System.out.println("Found " + p.toString());
             } catch (IOException e) {
                 System.err.println(e.getMessage());
                 System.exit(-1);
             }
         }
     }
-    private static Path findFilesForId(String path, String regex) throws IOException {
+    private static Path findFileForId(String path, String regex) throws IOException {
         List<Path> paths = Files.find(Paths.get(path), 16,
             (file, attr) -> file.toString().matches(regex))
             .collect(Collectors.toList());
