760d58cc5813186bd17d842c7f44fad63ae19f16
Zeynep Bayazitoglu <zeynep.bayazitoglu@ergosoft.co.uk>
2021-05-07 11:39:42 +0100

    fix duplication in concept_search table

--- Database/7.IMv6/04.PopulateConceptSearch.sql
+++ Database/7.IMv6/04.PopulateConceptSearch.sql
@@ -1,15 +1,8 @@
-USE IM6;
 
 LOCK TABLES concept_search WRITE, concept READ, term_code READ;
 
 INSERT INTO concept_search(term, concept_dbid)
-SELECT iri, dbid FROM concept;
-
-INSERT INTO concept_search(term, concept_dbid)
 SELECT code, dbid FROM concept;
-
-INSERT INTO concept_search(term, concept_dbid)
-SELECT name, dbid FROM concept;
 
 INSERT INTO concept_search(term, concept_dbid)
 SELECT term, concept FROM term_code;
